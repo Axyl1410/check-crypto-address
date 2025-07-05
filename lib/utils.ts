4ac1509cd564f32aa85bd52aa87a9fed79d6f85d
Alex <truonggiang190689@gmail.com>
2025-07-05 15:00:55 +0700

    first commit

---
+++ lib/utils.ts
@@ -0,0 +1,23 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const formatAddress = (address: string) => {
+  return `${address.substring(0, 6)}...${address.substring(
+    address.length - 4,
+  )}`;
+};
+
+export function assertValue<T>(
+  v: T | undefined | null,
+  errorMessage: string,
+): T {
+  if (v === undefined || v === null) {
+    throw new Error(errorMessage);
+  }
+
+  return v;
+}
